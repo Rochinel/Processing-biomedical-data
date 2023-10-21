af1e9113b53771d38091b5a4257811d7c871d2f0
rochinel <bienvenunanfa@gmail.com>
2023-10-21 17:47:36 +0200

    ajout d'un nouveau corpus de test

--- Exemple/main.py
+++ Exemple/main.py
@@ -19,6 +19,14 @@ def main():
             "She denied preceding symptoms of chest discomfort, palpitations, syncope or infection. "
             "The patient was afebrile and normotensive, with a sinus tachycardia of 140 beats/min, corona, malaria")
 
+    doc = """
+    	CASE: A 28-year-old previously healthy man presented with a 6-week history of palpitations. 
+          The symptoms occurred during rest, 2–3 times per week, lasted up to 30 minutes at a time 
+          and were associated with dyspnea. Except for a grade 2/6 holosystolic tricuspid regurgitation 
+          murmur (best heard at the left sternal border with inspiratory accentuation), physical 
+          examination yielded unremarkable findings.
+          """
+
 
 
     # list of entities we want to detecte
@@ -33,8 +41,10 @@ def main():
 
     # lets analyse our corpus now
     summary = corpus_analyzer(pipe=pipe, corpus=data)
+    summary_2 = corpus_analyzer(pipe=pipe, corpus=doc)
 
-    print('Corpus_Analyser: ', summary)
+    print('Corpus_Analyser_1: ', summary)
+    print('corpus_Analyse_2: ', summary_2)
 
 
 
